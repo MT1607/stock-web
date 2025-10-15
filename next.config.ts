03b754f625ed37f18dbdc6bea1fda0ef0b0ab5ab
MT1607 <nguyengiason5@gmail.com>
2025-10-15 22:48:13 +0700

    Init project

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/stock",
+  async redirects() {
+    return [
+      {
+        source: '/', 
+        destination: '/stock', 
+        basePath: false, // ⬅️ Rất quan trọng để tránh lỗi /stock/stock
+        permanent: true, // ⬅️ Nên dùng 'true' trong Production
+      },
+    ];
+  },
 };
 
 export default nextConfig;
